6fc9d4119e4d6b49d206bf679a5067344c47c659
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2019-06-30 00:42:02 +0900

    Broadcast via job

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,7 +1,7 @@
 class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(message)
+    ActionCable.server.broadcast 'chat_channel', message.to_chat_hash
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,22 @@
 require 'digest/md5'
 
 class Message < ApplicationRecord
-  validate :name, presence: true
-  validate :body, presence: true
-  validate :sent_at, presence: true
+  validates :name, presence: true
+  validates :body, presence: true
+  validates :sent_at, presence: true
 
   def avatar
     "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(name.to_s)}?d=identicon&f=y"
+  end
+
+  def to_chat_hash
+    {
+      id: id,
+      name: name,
+      body: body,
+      sent_at: sent_at,
+      avatar: avatar,
+    }
   end
 
   after_create_commit do

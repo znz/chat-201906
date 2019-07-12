e274a3777c0b03c675919b5d020b20fd6d5036f5
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2019-07-13 08:16:43 +0900

    Set `null: false`

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,2 +1,5 @@
 class Appearance < ApplicationRecord
+  validates :remote_ip, presence: true
+  validates :request_id, presence: true
+  validates :name, presence: true
 end
--- db/migrate/20190712231532_create_appearances.rb
+++ db/migrate/20190712231532_create_appearances.rb
@@ -1,9 +1,9 @@
 class CreateAppearances < ActiveRecord::Migration[6.0]
   def change
     create_table :appearances do |t|
-      t.string :remote_ip
-      t.string :request_id
-      t.string :name
+      t.string :remote_ip, null: false
+      t.string :request_id, null: false
+      t.string :name, null: false
 
       t.timestamps
     end
